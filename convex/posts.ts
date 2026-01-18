a4cd9782eb9ca6903a79c899b433ad7baef04942
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-19 02:51:47 +0530

    Implemented getfeedpost function to read the post liked and bookmark by the user and interaction

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,5 +1,6 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { getAuthenticatedUser } from "./users";
 
 export const generateUploadUrl = mutation(async(ctx)=>{
     const identiy = await ctx.auth.getUserIdentity();
@@ -15,14 +16,7 @@ export const createPost = mutation({
     },
 
     handler: async (ctx,args) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) throw new Error("Unathorized");
-
-        const currentUser = await ctx.db.query("users").withIndex(
-            "by_clerk_id", (q) => q.eq("clerkId", identity.subject)
-        ).first()
-
-        if (!currentUser) throw new Error("User not found");
+        const currentUser = await getAuthenticatedUser(ctx);
 
         const imageUrl = await ctx.storage.getUrl(args.storageId);
         if (!imageUrl) throw new Error("Image not found");
@@ -45,4 +39,39 @@ export const createPost = mutation({
 
         return postId;
     }
+})
+
+export const getFeedPosts = query({
+    handler: async (ctx) => {
+        const currentUser = await getAuthenticatedUser(ctx);
+
+        //Get all posts
+        const posts = await ctx.db.query("posts").order("desc").collect();
+
+        if (posts.length === 0) return [];
+
+        //enhance post with user data and interaction status
+        const postsWithInfo = await Promise.all(posts.map(async(post) => {
+            const postAuthor = await ctx.db.get(post.userId);
+
+            const like = await ctx.db.query("likes")
+            .withIndex("by_user_and_post", (q)=>q.eq("userId", currentUser._id).eq("postId", post._id)).first();
+
+            const bookmark = await ctx.db.query("bookmarks")
+            .withIndex("by_user_and_post", (q)=>q.eq("userId", currentUser._id).eq("postId", post._id)).first();
+
+            return {
+                ...post,
+            author: {
+            _id:postAuthor?._id,
+            username: postAuthor?.username,
+            image: postAuthor?.image,
+            },
+            isLiked: !!like,
+            isBookmarked: !!bookmark,
+        }
+        })
+    )
+        return postsWithInfo;
+    },
 })
