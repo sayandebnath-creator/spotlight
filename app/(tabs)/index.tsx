6ee0ace4c86e9e82c733fb6c4116acb4330ce4e1
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-19 03:57:11 +0530

    Added ux like not found,loader in home page and some padding in content containerstyle with post component fetching hardcoded data with dynamix image of user,name and the post with the caption

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,10 +6,20 @@ import { COLORS } from "@/constants/theme";
 import { Ionicons } from "@expo/vector-icons";
 import { STORIES } from "@/constants/mock-data";
 import Story from "@/components/Story";
+import { useQuery } from "convex/react";
+import { Loader } from "@/components/Loader";
+import { api } from "@/convex/_generated/api";
+import Post from "@/components/Post";
 
 export default function Index() {
   
   const {signOut} =useAuth()
+
+  const posts = useQuery(api.posts.getFeedPosts);
+
+  if (posts === undefined) return <Loader/>
+
+  if (posts.length === 0) return <NoPostsFound/>
 
   return (
     <View
@@ -22,14 +32,31 @@ export default function Index() {
           <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
         </TouchableOpacity>
       </View>
-      <ScrollView showsVerticalScrollIndicator={false} horizontal style={styles.storiesContainer}>
-        {/* stories */}
-        {STORIES.map((story)=>(
-          <Story key={story.id} story={story} />
-        ))}
-
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 60}}>
+        <ScrollView showsVerticalScrollIndicator={false} horizontal style={styles.storiesContainer}>
+          {/* stories */}
+          {STORIES.map((story)=>(
+            <Story key={story.id} story={story} />
+          ))}
+        </ScrollView>
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
       </ScrollView>
     </View>
   );
 }
+
+const NoPostsFound = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+  </View>
+);
 
