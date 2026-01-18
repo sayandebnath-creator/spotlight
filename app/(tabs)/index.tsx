1c678f8d67d4f21b610f30a26089b74bf46c0d48
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-19 03:18:08 +0530

    Added hardcoded mock data for stories with styling and a scroll horizontal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View, TouchableOpacity, Pressable, Image } from "react-native";
-import { styles } from "../../styles/auth.styles"
+import { Text, View, TouchableOpacity, Pressable, Image, ScrollView } from "react-native";
+import { styles } from "../../styles/feed.styles"
 import { Link } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { COLORS } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
 
 export default function Index() {
   
@@ -11,11 +15,20 @@ export default function Index() {
     <View
       style={styles.container}
     >
-      <TouchableOpacity onPress={()=>signOut()}>
-        <Text style={{color: "white"}}>
-          Signout
-        </Text>
-      </TouchableOpacity>
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Spotlight</Text>
+        <TouchableOpacity onPress={()=>signOut()}>
+          <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false} horizontal style={styles.storiesContainer}>
+        {/* stories */}
+        {STORIES.map((story)=>(
+          <Story key={story.id} story={story} />
+        ))}
+
+      </ScrollView>
     </View>
   );
 }
