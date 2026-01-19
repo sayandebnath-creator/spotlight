1d45b65cfb93d6ec970e63ad6a7c3fdbb455a106
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-20 01:39:21 +0530

    Added notification component with styles,notification component separate from tab, api to fetch all notifications, implemented notifications to be deleted with the delet of post

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { Loader } from '@/components/Loader';
+import { Ionicons } from '@expo/vector-icons';
+import { styles } from '@/styles/notifications.styles';
+import { COLORS } from '@/constants/theme';
+import Notification from '@/components/Notification';
 
 export default function Notifications() {
+  const notifications = useQuery(api.notifications.getNotifications);
+
+  if (notifications === undefined) return <Loader/>;
+  if (notifications.length === 0) return <NoNotificationsFound/>;
+
   return (
-    <View>
-      <Text>Notifications Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Notifications</Text>
+      </View>
+
+      <FlatList
+        data={notifications}
+        renderItem ={({item}) => <Notification notification={item} />}
+        keyExtractor={(item) => item._id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 60}}
+      />
     </View>
   )
+}
+
+function NoNotificationsFound() {
+  return (
+    <View style={[styles.container, styles.centered]}>
+      <Ionicons name="notifications-outline" size={48} color={COLORS.primary} />
+      <Text style={{ fontSize: 20, color: COLORS.white }}>No notifications yet</Text>
+    </View>
+  );
 }
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -11,6 +11,7 @@
 import type * as bookmarks from "../bookmarks.js";
 import type * as comments from "../comments.js";
 import type * as http from "../http.js";
+import type * as notifications from "../notifications.js";
 import type * as posts from "../posts.js";
 import type * as users from "../users.js";
 
@@ -24,6 +25,7 @@ declare const fullApi: ApiFromModules<{
   bookmarks: typeof bookmarks;
   comments: typeof comments;
   http: typeof http;
+  notifications: typeof notifications;
   posts: typeof posts;
   users: typeof users;
 }>;
