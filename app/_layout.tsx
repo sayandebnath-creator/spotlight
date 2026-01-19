1f07295afb3292b8c20ce7d1804ca65c6cfe2983
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-19 21:11:41 +0530

    Added new splashicon and loaded the custom font downloaded from jetbrains

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import InitialLayout from "@/components/InitialLayout";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
 
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }} onLayout={onLayoutRootView}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
