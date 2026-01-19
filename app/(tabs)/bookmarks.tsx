70b44fdbf8e6d1edc0be4150b9e5927c3ac3478d
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-20 00:58:41 +0530

    Added bookmark component

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { Image } from 'expo-image';
+import { COLORS } from '@/constants/theme';
+import { styles } from '@/styles/feed.styles';
+import { Loader } from '@/components/Loader';
 
 export default function bookmarks() {
+  const bookmarkedPosts = useQuery(api.bookmarks.getBookmarkedPosts);
+
+  if (bookmarkedPosts === undefined) return <Loader />;
+  if (bookmarkedPosts.length === 0) return <NoBookmarksFound />;
+  
   return (
-    <View>
-      <Text>bookmarks</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Bookmarks</Text>
+        </View>
+
+        {/* POSTS */}
+        <ScrollView
+          contentContainerStyle={{
+            padding: 8,
+            flexDirection: "row",
+            flexWrap: "wrap",
+          }}
+        >
+          {bookmarkedPosts.map((post) => {
+            if (!post) return null;
+            return (
+              <View key={post._id} style={{ width: "33.33%", padding: 1 }}>
+                <Image
+                  source={post.imageUrl}
+                  style={{ width: "100%", aspectRatio: 1 }}
+                  contentFit="cover"
+                  transition={200}
+                  cachePolicy="memory-disk"
+                />
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    );
+  }
+
+  function NoBookmarksFound() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: COLORS.background,
+        }}
+      >
+        <Text style={{ color: COLORS.primary, fontSize: 22 }}>No bookmarked posts yet</Text>
+      </View>
+    );
+  }
+
