2af1e05d0a0e2755d892d54699a6cf595c2736de
Sayan Debnath <sayandebnath@Sayans-MacBook-Air.local>
2026-01-20 15:19:40 +0530

    Solved nagivation of other user tab and added the ui

--- app/user/[id].tsx
+++ app/user/[id].tsx
@@ -1,23 +1,30 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, Pressable, FlatList } from 'react-native'
 import React from 'react'
 import { useMutation, useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Id } from '@/convex/_generated/dataModel';
 import { Loader } from '@/components/Loader';
 import { styles } from '@/styles/profile.styles';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS } from '@/constants/theme';
+import { Image } from 'expo-image';
 
 export default function UserProfileScreen() {
     const { id } = useLocalSearchParams();
+    const router = useRouter();
+
     const profile = useQuery(api.users.getUserProfile, {id: id as Id<"users">});
-    const posts = useQuery(api.users.getUserProfile, {id: id as Id<"users">});
+    const posts = useQuery(api.posts.getPostsByUser, {userId: id as Id<"users">});
     const isFollowing = useQuery(api.users.isFollowing, {followingId: id as Id<"users">});
     const toggleFollow = useMutation(api.users.toggleFollow);
 
     const handleBack = () => {
-        //navigation.goBack();
+        if(router.canGoBack()) { //router as attribute
+            router.back();
+        } else {
+            router.replace('/(tabs)'); // Navigate to back to home
+        }
     }
 
     if(profile === undefined || posts === undefined || isFollowing === undefined) return <Loader/>;
@@ -31,6 +38,65 @@ export default function UserProfileScreen() {
         <Text style={styles.headerTitle}>{profile.username}</Text>
         <View style={{ width: 24 }} />
       </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.profileInfo}>
+             <View style={styles.avatarAndStats}>
+                <Image
+                    source={profile.image}
+                    style={styles.avatar}
+                    contentFit='cover'
+                    cachePolicy='memory-disk'
+                />
+                    {/* STATS */}
+                    <View style={styles.statsContainer}>
+                    <View style={styles.statItem}>
+                        <Text style={styles.statNumber}>{profile.posts}</Text>
+                        <Text style={styles.statLabel}>Posts</Text>
+                    </View>
+                    <View style={styles.statItem}>
+                        <Text style={styles.statNumber}>{profile.followers}</Text>
+                        <Text style={styles.statLabel}>Followers</Text>
+                    </View>
+                    <View style={styles.statItem}>
+                        <Text style={styles.statNumber}>{profile.following}</Text>
+                        <Text style={styles.statLabel}>Following</Text>
+                    </View>
+                </View>
+             </View>
+            <Text style={styles.name}>{profile.fullname}</Text>
+            {profile.bio && <Text style={styles.bio}>{profile.bio}</Text>}
+
+            <Pressable style={[styles.followButton, isFollowing && styles.followingButton]}
+                onPress={() => toggleFollow({followingId: id as Id<"users">})}
+            >
+                <Text style={styles.followButtonText}>{isFollowing ? "Following" : "Follow"}</Text>
+            </Pressable>
+        </View>
+
+        <View style={styles.postsGrid}>
+            {posts.length === 0 ? (
+                <View style={styles.noPostsContainer}>
+                    <Ionicons name="image-outline" size={24} color={COLORS.grey} />
+                    <Text style={styles.noPostsText}>No posts yet</Text>
+                </View>
+            ) : (
+                <FlatList
+                data={posts}
+                numColumns={3}
+                renderItem={({ item }) => (
+                    <Image
+                        source={item.imageUrl}
+                        style={styles.gridImage}
+                        contentFit='cover'
+                        transition={200}
+                        cachePolicy='memory-disk'
+                    />
+                )}
+                />
+            )}
+        </View>
+      </ScrollView>
     </View>
   )
 }
